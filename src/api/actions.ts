d8bd12228e49a2c23623e66b2d91066e258bb3a5
Rania22378 <159443513+Rania22378@users.noreply.github.com>
2024-05-27 20:21:41 +0000

    Initial commit

--- src/api/actions.ts
+++ src/api/actions.ts
@@ -1,7 +1,6 @@
 import axios, { AxiosError } from "axios";
 
-const API_URL = "https://studious-train-7pq794x5gr72xrrv-3000.app.github.dev/api";
-
+const API_URL = "https://redesigned-capybara-wrv75w6qj7vxh97g6-8080.app.github.dev/api"
 export const getWeatherData = async (city: string): Promise<WeatherData> => {
   return new Promise<WeatherData>((resolve, reject) => {
     axios
